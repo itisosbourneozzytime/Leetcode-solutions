d978bd35bb05e3194a60f4cac32b203342d57b38
Jane Frank <khusnutdinova.e@gmail.com>
2024-07-09 12:09:12 +0500

    Add pandas solution for 1045.

--- SQL/leetcode_pandas_1045.py
+++ SQL/leetcode_pandas_1045.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
 def find_customers(customer: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
-    pr_len = len(product)
     data = customer.groupby('customer_id')['product_key'].count().reset_index()
-    data = data[data['product_key'] == pr_len][['customer_id']]
-    return data
+    return data.loc[data.product_key == len(product)][['customer_id']]
